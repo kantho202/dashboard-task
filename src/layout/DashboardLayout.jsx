40f06e7e9dec8f80c98dfb37f68cceb909829ae0
kantho202 <kanthochakma2025@gmail.com>
2026-02-26 22:46:43 +0600

    dashboardLayout-update

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,60 +1,106 @@
 import React from 'react';
 import { MdDashboard, MdNotificationsNone, MdOutlineEmail } from 'react-icons/md';
+import { Link, Outlet } from 'react-router';
 
 const DashboardLayout = () => {
     return (
-        <div className='p-5'>
-             <div className='grid grid-cols-12 gap-5 w-full '>
-                <div className='col-span-2 border-2 border-red-500'>
-                    <h1 className='font-bold text-2xl p-5'>Donezo</h1>
-                    <div>
-                        <h1 className='font-semibold py-3'>Menu</h1>
-                        <h1 className='flex items-center gap-2 font-semibold'> <MdDashboard /> Dashboard</h1>
-                    </div>
-                </div>
-                <div className='col-span-10 border-2 border-amber-500'>
-                    {/* searchbox */}
-                    <div className='flex gap-4 items-center justify-between'>
-                        <div>
-                            <label className="input outline-0 ">
-                                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                    <g
-                                        strokeLinejoin="round"
-                                        strokeLinecap="round"
-                                        strokeWidth="2.5"
-                                        fill="none"
-                                        stroke="currentColor"
-                                    >
-                                        <circle cx="11" cy="11" r="8"></circle>
-                                        <path d="m21 21-4.3-4.3"></path>
-                                    </g>
-                                </svg>
-                                <input type="search" required placeholder="Search" />
-                            </label>
-                        </div>
-                        <div className='flex gap-5'>
-                            <div className='flex gap-4 items-center'>
-                                <MdOutlineEmail size={23} />
-                                <MdNotificationsNone size={23} />
-                            </div>
-                            <div className='flex gap-2.5'>
-                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                    <div className="w-10 rounded-full">
-                                        <img
-                                            alt="Tailwind CSS Navbar component"
-                                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                                    </div>
-                                </div>
-                                <div>
-                                    <h1 className='font-bold'>Hanney Shing</h1>
-                                    <p className='font-medium text-gray-500 text-[13px]'>hanneyshing@gmail.com</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className="drawer lg:drawer-open">
+  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    {/* Navbar */}
+    <nav className="navbar w-full bg-base-300">
+      <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost flex lg:hidden">
+        {/* Sidebar toggle icon */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
+      </label>
+      <div className="px-4">Navbar Title</div>
+    </nav>
+    {/* Page content here */}
+    <div className="p-4">Page Content</div>
+  </div>
+
+  <div className="drawer-side is-drawer-close:overflow-visible ">
+    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
+    <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-24 is-drawer-open:w-64">
+      {/* Sidebar content here */}
+      <ul className="menu w-full grow">
+        {/* List item */}
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Homepage">
+            {/* Home icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path><path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
+            <span className="is-drawer-close:hidden">Homepage</span>
+          </button>
+        </li>
+
+        {/* List item */}
+        <li>
+          <button className="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
+            {/* Settings icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M20 7h-9"></path><path d="M14 17H5"></path><circle cx="17" cy="17" r="3"></circle><circle cx="7" cy="7" r="3"></circle></svg>
+            <span className="is-drawer-close:hidden">Settings</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+        // <div className='p-5'>
+        //      <div className='grid grid-cols-12 gap-5 w-full '>
+        //         <div className='col-span-2 border-2 border-red-500'>
+        //             <h1 className='font-bold text-2xl p-5'>Donezo</h1>
+        //             <div>
+        //                 <h1 className='font-semibold py-3'>Menu</h1>
+        //                 <Link to='/' className='flex items-center gap-2 font-semibold'> <MdDashboard /> Dashboard</Link>
+        //                 <Link to='/products' className='flex items-center gap-2 font-semibold'> <MdDashboard /> Products</Link>
+        //             </div>
+                    
+        //         </div>
+        //         <div className='col-span-10 border-2 border-amber-500'>
+        //             {/* searchbox */}
+        //             <div className='flex gap-4 items-center justify-between'>
+        //                 <div>
+        //                     <label className="input outline-0 ">
+        //                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        //                             <g
+        //                                 strokeLinejoin="round"
+        //                                 strokeLinecap="round"
+        //                                 strokeWidth="2.5"
+        //                                 fill="none"
+        //                                 stroke="currentColor"
+        //                             >
+        //                                 <circle cx="11" cy="11" r="8"></circle>
+        //                                 <path d="m21 21-4.3-4.3"></path>
+        //                             </g>
+        //                         </svg>
+        //                         <input type="search" required placeholder="Search" />
+        //                     </label>
+        //                 </div>
+        //                 <div className='flex gap-5'>
+        //                     <div className='flex gap-4 items-center'>
+        //                         <MdOutlineEmail size={23} />
+        //                         <MdNotificationsNone size={23} />
+        //                     </div>
+        //                     <div className='flex gap-2.5'>
+        //                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        //                             <div className="w-10 rounded-full">
+        //                                 <img
+        //                                     alt="Tailwind CSS Navbar component"
+        //                                     src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        //                             </div>
+        //                         </div>
+        //                         <div>
+        //                             <h1 className='font-bold'>Hanney Shing</h1>
+        //                             <p className='font-medium text-gray-500 text-[13px]'>hanneyshing@gmail.com</p>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //             <div className="p-4"><Outlet></Outlet> </div>
+        //         </div>
+                
+        //     </div>
+        // </div>
     );
 };
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,10 +2,17 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import DashboardLayout from "../layout/DashboardLayout";
+import Products from "../components/Products/Products";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element:<DashboardLayout></DashboardLayout>
+    element:<DashboardLayout></DashboardLayout>,
+    children:[
+      {
+        path:'/products',
+        element:<Products></Products>
+      }
+    ]
   },
 ]);
